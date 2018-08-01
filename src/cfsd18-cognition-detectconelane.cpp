15bb9e31406137fc15ba6cf0385cbd5c3b3d0817
erjonas1 <erjonas@student.chalmers.se>
2018-08-01 13:39:42 +0200

    Updated stamp settings

--- src/cfsd18-cognition-detectconelane.cpp
+++ src/cfsd18-cognition-detectconelane.cpp
@@ -55,12 +55,14 @@ int32_t main(int32_t argc, char **argv) {
     uint32_t slamStamp = (commandlineArguments.count("slamId")>0)?(static_cast<uint32_t>(std::stoi(commandlineArguments["slamId"]))):(120);
     uint32_t attentionStamp = (commandlineArguments.count("attentionId")>0)?(static_cast<uint32_t>(std::stoi(commandlineArguments["attentionId"]))):(116);
     uint32_t simDetectconeStamp = (commandlineArguments.count("simDetectConeId")>0)?(static_cast<uint32_t>(std::stoi(commandlineArguments["simDetectConeId"]))):(231);
-    uint32_t estimationDefault = (accelerationMode)?(112):(114);
-    uint32_t estimationStamp = (commandlineArguments.count("estimationId")>0)?(static_cast<uint32_t>(std::stoi(commandlineArguments["estimationId"]))):(estimationDefault);
+    uint32_t yawRateStamp = (commandlineArguments.count("yawRateId")>0)?(static_cast<uint32_t>(std::stoi(commandlineArguments["yawRateId"]))):(114);
+    uint32_t gpsId = (commandlineArguments.count("gpsId")>0)?(static_cast<uint32_t>(std::stoi(commandlineArguments["gpsId"]))):(114);
+    bool useRawGPS = (commandlineArguments["useRawGPS"].size() != 0) ? (std::stoi(commandlineArguments["useRawGPS"])==1) : (false);
+    uint32_t gpsStamp = (useRawGPS) ? (108) : (gpsId);
     uint32_t speedStamp = 114;
     uint32_t id = (commandlineArguments.count("id")>0)?(static_cast<uint32_t>(std::stoi(commandlineArguments["id"]))):(211);
 
-    auto poseEnvelope{[&detectconelane,senderStamp = estimationStamp](cluon::data::Envelope &&envelope)
+    auto poseEnvelope{[&detectconelane,senderStamp = gpsStamp](cluon::data::Envelope &&envelope)
       {
         if(envelope.senderStamp() == senderStamp){
           detectconelane.nextPos(envelope);
@@ -77,7 +79,7 @@ int32_t main(int32_t argc, char **argv) {
       } 
     };
 
-    auto yawRateEnvelope{[&detectconelane, senderStamp = estimationStamp](cluon::data::Envelope &&envelope)
+    auto yawRateEnvelope{[&detectconelane, senderStamp = yawRateStamp](cluon::data::Envelope &&envelope)
       {
         if(envelope.senderStamp() == senderStamp){
           detectconelane.nextYawRate(envelope);
